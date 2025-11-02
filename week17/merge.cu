8fb5cc74cb4ace46b98c7cd486ca5df0eba37fbc
punsnx <79169700+punsnx@users.noreply.github.com>
2025-11-02 17:05:37 +0700

    just update

---
+++ week17/merge.cu
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+__global__ void merge(){
+
+}
+
+int corank(int k,int *A,int *B,int n){
+    int i_min = max(0,k-n);
+    int i_max = min(k,k-n);
+    
+}
+
+int main(){
+
+
+
+
+    return 0;
+}
